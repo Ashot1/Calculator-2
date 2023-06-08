f775cbdcb1f8deb3d03a568d2542c051be2ea6b9
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2023-06-09 01:36:51 +0300

    2.0.11

--- src/APP/App.tsx
+++ src/APP/App.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
                   errorElement: <ErrorPage/>
             },
             {
-                path: "*",
+                path: ":lang/*",
                 element: <NotFoundedPage/>
             }
         ]
--- src/PAGES/NotFoundedPage/NotFoundedPage.tsx
+++ src/PAGES/NotFoundedPage/NotFoundedPage.tsx
@@ -1,11 +1,18 @@
 import styles from './NotFounded.module.sass'
 import { FC } from 'react'
+import { NotFoundedText } from '../../Localisation'
+import { Navigate, useParams } from 'react-router-dom'
 
 const NotFoundedPage: FC = () => {
+	const params = useParams()
+	let lang = params.lang
+	if(lang === undefined) lang = navigator.language
+
 
 	return (
 		<h1 className={styles.huy}>
-			Page not found
+			<Navigate to={`/${lang}/NotFound`} />
+			{NotFoundedText(lang)}
 		</h1>
 	)
 }
